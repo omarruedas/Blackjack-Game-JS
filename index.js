26e960801cb31f65becc1ca4ef66c633890a0f91
omarruedas <omarruedas.or@gmail.com>
2022-11-23 14:54:39 -0800

    second commit

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
-let firstCard = 10
-let secondCard = 11
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Per",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
-let messageEl = document.getElementById('message-el')
-let sumEl = document.querySelector('#sum-el')
-let cardsEl = document.querySelector('#cards-el')
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
+
 function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want a new card?"
-    }
-    else if (sum === 21) {
-        message = "You've got a Blackjack!"
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
         hasBlackJack = true
-    }
-    else if(sum > 21) {
+    } else {
         message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
-    sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
 }
+
 
 function newCard() {
-    let card = 5
-    sum += card
-    cards.push(card)
-    renderGame()
-}
-
-let messages = [
-    "Hey, hows it going",
-    "Im good thank you, and you?",
-    "All good. Been working on my portfolio lately",
-    "Same here"
-]
-
-for (let i = 0; i < 5; i += 1) {
-    console.log(messages[i])
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
